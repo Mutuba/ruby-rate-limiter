9426464256594583ef5ccd8823e5d770b861d4e9
Mutuba <danielmutubait@gmail.com>
2024-07-15 12:30:45 +0300

    Require necessary files

--- lib/ruby_rate_limiter.rb
+++ lib/ruby_rate_limiter.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 require_relative "ruby_rate_limiter/version"
+require 'ruby_rate_limiter/token_bucket'
+require 'ruby_rate_limiter/storage/abstract_storage'
+require 'ruby_rate_limiter/storage/redis_storage'
+
 
 module RubyRateLimiter
   class Error < StandardError; end
