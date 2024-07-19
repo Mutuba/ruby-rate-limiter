33f4c4dc756da0b4c39b9934f85ae64f84d8a3ba
Mutuba <danielmutubait@gmail.com>
2024-07-19 08:45:26 +0300

    Code clean up

--- lib/ruby_rate_limiter.rb
+++ lib/ruby_rate_limiter.rb
@@ -5,7 +5,6 @@ require 'ruby_rate_limiter/token_bucket'
 require 'ruby_rate_limiter/storage/abstract_storage'
 require 'ruby_rate_limiter/storage/redis_storage'
 
-
 module RubyRateLimiter
   class Error < StandardError; end
 end
